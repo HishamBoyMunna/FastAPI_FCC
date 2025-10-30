690f45d4e6cbc06c275a607ee24bca104e6722e6
HishamBoyMunna <158536380+HishamBoyMunna@users.noreply.github.com>
2025-10-30 13:33:28 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/bin/env python3.13
+
+import contextlib
+from fastapi import FastAPI, HTTPException, Query
+from pymongo import MongoClient
+from bson import ObjectId
+from fastapi.encoders import jsonable_encoder
+
+app = FastAPI()
+client = MongoClient("mongodb://localhost:27017/")
+db = client["courses"]
